ff61ff500cd0055b73ca81daa09a24d0497f46b3
Willy Lee <willytylee@gmail.com>
2022-08-18 16:27:48 +0800

    update the version number

---
+++ scripts/post-build.js
@@ -0,0 +1,20 @@
+import { writeFile } from "fs";
+
+const d = new Date();
+const year = d.getFullYear().toString();
+const month = (d.getMonth() + 1).toString();
+const date = d.getDate().toString();
+const hour = d.getHours().toString();
+const minutes = d.getMinutes().toString();
+
+// version=${year:3:1}.${month}${date}.${time:0:3}
+const version = `${year.slice(-1)}.${month}${date}.${hour}${minutes.slice(
+  -2,
+  -1
+)}`;
+
+writeFile("./build/version.json", version, (err) => {
+  if (err) {
+    console.error(err);
+  }
+});
