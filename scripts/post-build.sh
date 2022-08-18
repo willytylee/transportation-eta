2e1f38a41f2ef14a5d5e3c706dc45fac1adf6b4a
Willy Lee <willytylee@gmail.com>
2022-08-18 16:09:45 +0800

    Setup post build

--- scripts/post-build.sh
+++ scripts/post-build.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-cp -rf /projects/eta/eta_app/build/* /var/www/eta.willytylee.com/
-
 year="$(date '+%Y')"
 month="$(date +'%-m')"
 day="$(date +'%-d')"
 time="$(date +'%H%M')"
 version=${year:3:1}.${month}${day}.${time:0:3}
-echo ${version} > /var/www/eta.willytylee.com/version.json
+echo ${version} > ./build/
