202de5e584e7ad4abb2179dab6aa6633638a5fb7
Willy Lee <willytylee@gmail.com>
2025-06-18 00:58:48 +0800

    fix style

--- src/components/StationMode/StationModeTabPanel.js
+++ src/components/StationMode/StationModeTabPanel.js
@@ -1,5 +1,7 @@
 import { useState, useEffect, useContext } from "react";
+import { styled } from "@mui/material";
 import { DbContext } from "../../context/DbContext";
+import { companyColor } from "../../constants/Constants";
 import { getLocalStorage, setLocalStorage, fetchBusEta, handleTableResult } from "../../Utils/Utils";
 import { dataSet } from "../../data/DataSet";
 
@@ -36,31 +38,80 @@ export const StationModeTabPanel = ({ value, index, stationModeData }) => {
   const result = handleTableResult(sectionData);
 
   return (
-    <div role="tabpanel" hidden={value !== index} id={`simple-tabpanel-${index}`}>
-      {value === index && (
-        <div>
-          <div className="row">
-            <div className="route title">路線</div>
-            <div className="dest title">總站</div>
-            <div className="etaWrapper title">分鐘</div>
-          </div>
-          {result.map((e, i) => (
-            <div className="row" key={i}>
-              <div className="route">
-                <span className={`${e.co}`}>{e.route}</span>
-              </div>
-              <div className="dest">{section[i].dest}</div>
-              <div className="etaWrapper">
-                {e.etas.slice(0, 2).map((eta, j) => (
-                  <div key={j} className={`eta eta${j}`}>
-                    {eta}
-                  </div>
-                ))}
-              </div>
+    <StationModeTabPanelRoot>
+      <div role="tabpanel" hidden={value !== index} id={`simple-tabpanel-${index}`}>
+        {value === index && (
+          <div>
+            <div className="row">
+              <div className="route title">路線</div>
+              <div className="dest title">總站</div>
+              <div className="etaWrapper title">分鐘</div>
             </div>
-          ))}
-        </div>
-      )}
-    </div>
+            {result.map((e, i) => (
+              <div className="row" key={i}>
+                <div className="route">
+                  <span className={`${e.co}`}>{e.route}</span>
+                </div>
+                <div className="dest">{section[i].dest}</div>
+                <div className="etaWrapper">
+                  {e.etas.slice(0, 2).map((eta, j) => (
+                    <div key={j} className={`eta eta${j}`}>
+                      {eta}
+                    </div>
+                  ))}
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </StationModeTabPanelRoot>
   );
 };
+
+const StationModeTabPanelRoot = styled("div")({
+  width: "100%",
+  paddingTop: "8px",
+  ".error": {
+    color: "#808080",
+    textDecoration: "line-through",
+  },
+  ".emptyMsg": {
+    fontSize: "14px",
+    textAlign: "center",
+    padding: "14px",
+  },
+  ".stationTitle": {
+    fontSize: "1.5rem",
+    textAlign: "center",
+    fontWeight: 900,
+    paddingBottom: "8px",
+  },
+  ".row": {
+    fontSize: "1.5rem",
+    display: "flex",
+    alignItems: "baseline",
+    padding: "8px",
+    borderBottom: "1px solid lightgrey",
+    ".title": {
+      fontWeight: "900",
+    },
+    ".route": {
+      ...companyColor,
+      width: "20%",
+      fontWeight: "900",
+    },
+    ".dest": {
+      width: "50%",
+    },
+    ".etaWrapper": {
+      width: "30%",
+      display: "flex",
+      alignItems: "flex-end",
+      flexDirection: "column",
+      ".eta1": {
+        fontSize: "15px",
+      },
+    },
+  },
+});
--- src/pages/StationMode.js
+++ src/pages/StationMode.js
@@ -1,10 +1,10 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import { Box, Tab, Tabs, styled } from "@mui/material/";
+import { Box, Tab, Tabs } from "@mui/material/";
 import { DepartureBoard as DepartureBoardIcon } from "@mui/icons-material";
 import { getLocalStorage, setLocalStorage, a11yProps } from "../Utils/Utils";
 import { dataSet } from "../data/DataSet";
-import { companyColor } from "../constants/Constants";
+
 import { StationTitleDialog } from "../components/StationMode/StationTitleDialog";
 import { StationModeTabPanel } from "../components/StationMode/StationModeTabPanel";
 
@@ -33,103 +33,54 @@ export const StationMode = () => {
 
   return (
     <>
-      <StationModeView>
-        {bookmark ? (
-          transportData?.length > 0 ? (
-            stationMode?.length > 0 ? (
-              <Box sx={{ width: "100%" }}>
-                  <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-                    <Tabs value={tabIdx} onChange={handleChange} variant="scrollable" scrollButtons allowScrollButtonsMobile>
-                      {stationMode.map((e, i) => (
-                        <Tab label={e.title} {...a11yProps(i)} key={i} />
-                      ))}
-                    </Tabs>
-                  </Box>
+      {bookmark ? (
+        transportData?.length > 0 ? (
+          stationMode?.length > 0 ? (
+            <Box sx={{ width: "100%" }}>
+              <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+                <Tabs value={tabIdx} onChange={handleChange} variant="scrollable" scrollButtons allowScrollButtonsMobile>
                   {stationMode.map((e, i) => (
-                    <StationModeTabPanel value={tabIdx} index={i} key={i} stationModeData={e} />
+                    <Tab label={e.title} {...a11yProps(i)} key={i} />
                   ))}
+                </Tabs>
               </Box>
-            ) : (
-              <div className="emptyMsg">
-                <p>未有設定車站模式。</p>
-                <p>
-                  請先到<Link to="/search">路線搜尋</Link>, 選擇巴士路線, 再選擇巴士站, 然後新增書籤。
-                </p>
-                <p>
-                  點擊
-                  <DepartureBoardIcon fontSize="small" />
-                  將路線加入車站模式
-                </p>
-              </div>
-            )
+              {stationMode.map((e, i) => (
+                <StationModeTabPanel value={tabIdx} index={i} key={i} stationModeData={e} />
+              ))}
+            </Box>
           ) : (
             <div className="emptyMsg">
-              <p>未有書籤。</p>
+              <p>未有設定車站模式。</p>
               <p>
                 請先到<Link to="/search">路線搜尋</Link>, 選擇巴士路線, 再選擇巴士站, 然後新增書籤。
+              </p>
+              <p>
+                點擊
+                <DepartureBoardIcon fontSize="small" />
+                將路線加入車站模式
               </p>
             </div>
           )
         ) : (
           <div className="emptyMsg">
-            <p>未有書籤</p>
+            <p>未有書籤。</p>
             <p>
-              現有用戶, 請到<Link to="/settings">設定</Link>, 載入用戶書籤。
-            </p>
-            <p>
-              新用戶, 請先到<Link to="/search">路線搜尋</Link>, 選擇巴士路線, 再選擇巴士站, 然後新增書籤。
+              請先到<Link to="/search">路線搜尋</Link>, 選擇巴士路線, 再選擇巴士站, 然後新增書籤。
             </p>
           </div>
-        )}
-      </StationModeView>
+        )
+      ) : (
+        <div className="emptyMsg">
+          <p>未有書籤</p>
+          <p>
+            現有用戶, 請到<Link to="/settings">設定</Link>, 載入用戶書籤。
+          </p>
+          <p>
+            新用戶, 請先到<Link to="/search">路線搜尋</Link>, 選擇巴士路線, 再選擇巴士站, 然後新增書籤。
+          </p>
+        </div>
+      )}
       <StationTitleDialog title={title} dialogOpen={dialogOpen} setDialogOpen={setDialogOpen} />
     </>
   );
 };
-
-const StationModeView = styled("div")({
-  width: "100%",
-  paddingTop: "8px",
-  ".error": {
-    color: "#808080",
-    textDecoration: "line-through",
-  },
-  ".emptyMsg": {
-    fontSize: "14px",
-    textAlign: "center",
-    padding: "14px",
-  },
-  ".stationTitle": {
-    fontSize: "1.5rem",
-    textAlign: "center",
-    fontWeight: 900,
-    paddingBottom: "8px",
-  },
-  ".row": {
-    fontSize: "1.5rem",
-    display: "flex",
-    alignItems: "baseline",
-    padding: "8px",
-    borderBottom: "1px solid lightgrey",
-    ".title": {
-      fontWeight: "900",
-    },
-    ".route": {
-      ...companyColor,
-      width: "20%",
-      fontWeight: "900",
-    },
-    ".dest": {
-      width: "50%",
-    },
-    ".etaWrapper": {
-      width: "30%",
-      display: "flex",
-      alignItems: "flex-end",
-      flexDirection: "column",
-      ".eta1": {
-        fontSize: "15px",
-      },
-    },
-  },
-});
